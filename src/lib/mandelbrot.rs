d7da1be09256a4692f03b07fb25b4aa84264cb39
Natalie Ko-Ferrigno <natalie@natbookpro.local>
2022-11-26 16:41:57 +0000

    initial upload of the files

---
+++ src/lib/mandelbrot.rs
@@ -0,0 +1,188 @@
+use num::complex::Complex;
+use itertools_num::linspace;
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::sync::mpsc;
+use threadpool::ThreadPool;
+
+#[derive(Debug)]
+pub struct Parameters{
+    pub zoom: f64,
+
+    pub low_x: f64,
+
+    pub low_y: f64,
+
+    pub radius_x: f64,
+    pub radius_y: f64,
+
+    pub quality: isize,
+	pub bound: f64,
+}
+
+pub fn write_to_file(params: &Parameters) -> String{
+	let values = calculate(params);
+	values
+}
+
+/*
+fn talk_to_python() -> std::io::Result<()>{
+    let host = "127.0.0.1:";
+    let pixel_port = "65432";
+    let param_port = "65431";
+
+    let pixel_hostname = host.to_string() + pixel_port;
+    let param_hostname = host.to_string() + param_port;
+    
+    let mut stream = TcpStream::connect(pixel_hostname).expect("run the renderer smh");
+    
+    println!("i do be listening doe");
+    
+    loop{
+        
+         let results = calculate(300.0, -0.5, 0.0, 1.0, 1.0, 510);
+        println!("ok just sending the results over now");
+        stream.write(&results.as_bytes())?;
+        println!( "{:?}", get_params(&param_hostname));
+        break;
+    }
+    Ok(())
+}
+*/
+
+fn get_params(param_hostname: &str) {
+    let listener = TcpListener::bind(param_hostname).unwrap();
+    println!("connected");
+    let mut stream = match listener.accept() {
+        Ok((socket, addr)) => {
+            println!("new client: {:?}", addr);
+            socket
+        }
+        Err(e) => {
+            println!("couldn't get client: {:?}", e);
+            panic!("oh no");
+        }
+    };
+    let mut buf = [0; 32];
+    stream.read(&mut buf).expect("error reading the stream");
+    println!("{:?}", buf);
+}
+
+
+fn bounded(c: &Complex<f64>, iterations: isize, bound: f64) -> isize{
+
+    let mut z = Complex::new(0.0, 0.0);
+
+    let mut i = 0;
+    loop{
+        z = (z * z) + c;
+		// the path the code takes if it diverges to infinity
+        if z.norm() > bound {
+            return i;
+        }
+		// the path the code takes if it stays bounded
+        if i >= iterations{
+            return -1;
+        }
+        i+=1;
+    }
+}
+
+fn calculate(parameters: &Parameters) -> String {
+    let low_x = parameters.low_x;
+    let high_x = low_x + 2.0 * parameters.radius_x;
+
+    let low_y = parameters.low_y;
+    let high_y = low_y + 2.0 * parameters.radius_y;
+    
+    let width = ((high_x - low_x) * parameters.zoom) as usize;
+    let height = ((high_y - low_y) * parameters.zoom) as usize;
+
+    if width > 2000 {panic!("width really big");}
+    if height > 2000 {panic!("height really big");}
+
+    let reals = linspace::<f64>(low_x,high_x,width);
+    let mut results = String::new();
+    
+/*
+ideas for multithreading this
+have an array for each point that a called function will insert a value into
+after all of the handles are joined, have the main thread output a string like it currently does
+
+use structs and have each thread return a struct of the value, real, imaginary
+somehow order the structs
+make the string
+
+BEST IDEA: READ THE FUCKING RUST DOCS messages seem useful :)
+
+*/
+
+    for y in reals{
+        let cs = linspace::<f64>(low_y,high_y,height);
+        for x in cs{
+            let c = Complex::new(x, y);
+            
+            results = results + &bounded(&c, parameters.quality, parameters.bound).to_string();
+            results = results + ",";
+        }
+		results = results + "\n";
+    }
+    results = results + &width.to_string();
+    results = results + ",";
+    results = results + &height.to_string();
+    results = results + "d";
+    results
+}
+
+struct ValueAtPoint{
+	real: f64,
+	imag: f64,
+	value: isize,
+}
+
+pub fn multi_calculate(parameters: &Parameters) -> String {
+	let height = (2.0 * parameters.radius_x * parameters.zoom) as usize;
+	let width = (2.0 * parameters.radius_y * parameters.zoom) as usize;
+	
+	let mut storage = vec![vec![0]];
+	
+	let high_x = parameters.low_x + parameters.radius_x * 2.0;
+	let high_y = parameters.low_y + parameters.radius_y * 2.0;
+
+	let pool = ThreadPool::new(4);
+	let (tx, rx) = mpsc::channel();
+	
+    let reals = linspace::<f64>(parameters.low_x,high_x,width);
+	for x in reals{
+        let complexes = linspace::<f64>(parameters.low_y,high_y,height);
+		for y in complexes{
+			let tx = tx.clone();
+			let quality = parameters.quality;
+			let bound = parameters.bound;
+			pool.execute(move || {
+				let c = Complex::new(x, y);
+				let output = ValueAtPoint{
+					real: x,
+					imag: y, 
+					value: bounded(&c, quality, bound),
+				};
+				tx.send(output).expect("messages didnt send");
+			});
+		}
+	}
+	pool.join();
+	println!("pool dropping");
+	drop(pool);
+	let mut x = 0;
+	let mut last_imag = f64::MIN;
+	for thing in rx{
+		storage[x].push(thing.value);
+		if last_imag > thing.imag{
+			x += 1;
+			storage.push(vec![0]);
+		}
+		last_imag = thing.imag;
+	}
+	println!("{:?} done", storage);
+	String::from("cock")
+}
---
+++ src/main.rs
@@ -0,0 +1,233 @@
+extern crate mandelbrot;
+use eframe::egui;
+use egui_extras;
+use std::cmp::{min, max};
+
+fn main() {
+	/*
+	println!("{}", min(1,2));
+	let options = eframe::NativeOptions::default();
+	eframe::run_native(
+		"My egui App",
+		options,
+		Box::new(|_cc| Box::new(App::default())),
+    );
+	*/
+	test_multicalculate();
+}
+
+fn test_multicalculate() {
+	let params = mandelbrot::Parameters{
+		zoom: 70.0,
+		low_x: 0.013,
+		low_y: 0.286,
+		radius_x: 0.5,
+		radius_y: 0.5,
+		quality: 80,
+		bound: 1000.0,
+	};
+	let output = mandelbrot::multi_calculate(&params);
+	println!("{output}");
+			
+}
+
+struct App {
+	params: mandelbrot::Parameters,
+	gamma: usize,
+	//set: egui::Painter,
+}
+
+struct FourValues {
+	min_in: f64,
+	max_in: f64,
+	min_out: f64,
+	max_out: f64,
+}
+
+impl Default for App{
+	fn default() -> Self {
+		Self {
+			params: mandelbrot::Parameters{
+				zoom: 70.0,
+				low_x: 0.013,
+				low_y: 0.286,
+				radius_x: 2.0,
+				radius_y: 2.0,
+				quality: 80,
+				bound: 1000.0,
+	
+			},
+			gamma: 0,
+			//set: egui::Painter::new(),
+		}
+	}
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+			ui.label(format!("zoom: {}", self.params.zoom));
+			ui.horizontal(|ui| {
+				if ui.button("- zoom").clicked() {
+					self.params.zoom *= 4.0/5.0;
+					self.params.radius_x *= 5.0/4.0;
+					self.params.radius_y *= 5.0/4.0;
+				}
+				if ui.button("+ zoom").clicked() {
+					self.params.zoom *= 5.0/4.0;
+					self.params.radius_x *= 4.0/5.0;
+					self.params.radius_y *= 4.0/5.0;
+					self.params.quality += 1;
+				}
+			});
+			ui.label(format!("x: {}, y: {}", self.params.low_x, self.params.low_y));
+			ui.horizontal(|ui| {
+			if ui.button("left").clicked() {
+				self.params.low_y -= self.params.radius_y * 0.2;
+			}
+			ui.vertical(|ui| {
+				if ui.button("up").clicked() {
+					self.params.low_x -= self.params.radius_x * 0.2;
+				}
+				if ui.button("down").clicked() {
+					self.params.low_x += self.params.radius_x * 0.2;
+				}
+			});
+			if ui.button("right").clicked() {
+				self.params.low_y += self.params.radius_y * 0.2;
+			}
+			});
+			
+			ui.horizontal(|ui| {
+				if ui.button("increase window size").clicked() {
+					self.params.zoom *= 5.0/4.0;
+				}
+				if ui.button("decrease window size").clicked() {
+					self.params.zoom *= 4.0/5.0;
+				}
+			});
+			ui.horizontal(|ui| {
+				if ui.button("increase quality").clicked() {
+					self.params.quality += 10;
+					self.gamma += 4;
+				}
+				if ui.button("decrease quality").clicked() {
+					self.params.quality -= 10;
+					self.gamma = self.gamma.checked_sub(2).unwrap_or_else(|| 0);
+				}
+				ui.label(format!("{}", self.params.quality));
+			});
+			ui.horizontal(|ui| {
+				if ui.button("add one to zoom").clicked() {
+					self.params.zoom += 1.0;
+				}
+				if ui.button("take one from zoom").clicked() {
+					self.params.zoom-= 1.0;
+				}
+			});
+	
+			ui.horizontal(|ui| {
+				if ui.button("increase gamma").clicked() {
+					self.gamma += 2;
+				}
+				if ui.button("decrease gamma").clicked() {
+					self.gamma = self.gamma.checked_sub(2).unwrap_or_else(|| 0);
+				}
+				ui.label(format!("{}", self.gamma));
+			});
+			
+			ui.horizontal(|ui| {
+				if ui.button("increase bound").clicked() {
+					self.params.bound += 10.0;
+				}
+				if ui.button("decrease bound").clicked() {
+					self.params.bound -= 10.0;
+				}
+				ui.label(format!("{}", self.params.bound));
+			});
+			
+	
+			println!("{:?}\n {}", &self.params, self.gamma);
+			self.params.low_x -= self.params.radius_x;
+			self.params.low_y -= self.params.radius_y;
+			let display = egui_extras::image::RetainedImage::from_color_image("text", render_image(get_data(&self.params), self.gamma));
+			self.params.low_x += self.params.radius_x;
+			self.params.low_y += self.params.radius_y;
+			
+			display.show(ui);
+    	});    
+	}
+
+
+
+}
+
+fn lerp(vals: &FourValues, to_map: &usize) -> f64{
+	let value:f64 = *to_map as f64;
+	let numerator = vals.min_out * (vals.max_in - value) + vals.max_out * (value - vals.min_in);
+	let denominator = vals.max_in - vals.min_in;
+	numerator/denominator
+} 
+
+fn get_data(params: &mandelbrot::Parameters) -> String{
+	mandelbrot::write_to_file(params)
+}
+
+fn render_image(data: String, gamma: usize) -> egui::ColorImage{
+	let array = parse(data);
+	let width: u16 = array.len().try_into().unwrap_or_else(|_| u16::MAX);
+	let height: u16 = array[0].len().try_into().unwrap_or_else(|_| u16::MAX);
+
+	let mut image_buffer: Vec<u8> = vec!(); 
+	
+	let map_consts = FourValues{
+		min_in: 0.0,
+		max_in: 255.0,
+		min_out: 0.0,
+		max_out: std::f64::consts::PI,
+	};
+
+
+	for x in 0..width{
+		for y in 0..height{
+			let mut value: usize = array[x as usize][y as usize].into();
+			value = value.checked_sub(gamma).unwrap_or_else(|| 0);
+			let lerped = lerp(&map_consts, &value);			
+			let r: u8 = (lerped.sin() * 255.0) as u8;
+			//let r: u8 = (min(value, 255)).try_into().unwrap_or_else(|_| u8::MAX);
+			let g: u8 = (min(value, 255)).try_into().unwrap_or_else(|_| u8::MAX);
+			let b: u8 = (max(2 * value as isize - 255 , 0)).try_into().unwrap_or_else(|_| 254)+1;
+
+			image_buffer.push(r);
+			image_buffer.push(g);
+			image_buffer.push(b);
+			image_buffer.push(255);
+	
+		}
+	}
+	egui::ColorImage::from_rgba_unmultiplied([width as usize, height as usize], &image_buffer)
+}
+
+fn parse(data: String) -> Vec<Vec<u8>>{
+	let lines = data.split("\n").collect::<Vec<&str>>();
+	
+	let last_line = lines[lines.len()-1].split(",").collect::<Vec<&str>>();
+	let width = last_line[0].parse::<usize>().unwrap();
+	
+	let mut height = last_line[1].to_string();
+	height.pop().unwrap();
+	let height = height.parse::<usize>().unwrap();
+	
+	let mut out: Vec<Vec<u8>> = vec!();
+
+	for i in 0..width{
+		out.push(vec!());
+		let values = lines[i].split(",").collect::<Vec<&str>>();
+		for j in 0..height{
+			out[i].push(values[j].parse::<u8>().unwrap_or_else(|_| 0 ));
+		}
+	}
+	
+
+	out
+}
